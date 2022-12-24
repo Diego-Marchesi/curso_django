5ce09786d9f9f9be4530831baefdbb27233e35e2
Diego Marchesi <cbdiego.marchesi@gmail.com>
2022-12-24 10:27:37 -0300

    inicio da separação das páginas de receitas

--- recipes/views.py
+++ recipes/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # HHTP REQUEST
 def Home(request):
     return render(request, 'recipes/pages/home.html')
+
+
+def Recipe(request, id):
+    return render(request, 'recipes/pages/home.html')
