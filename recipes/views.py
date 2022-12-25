1e0acc2e8efdd4ffdebbc6fc64c7b7e847baeb6b
Diego Marchesi <cbdiego.marchesi@gmail.com>
2022-12-25 16:03:05 -0300

    injetando dados dinamicos nas receitas

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 
+# import fake para criação altomatica e testes
+from utils.recipes.factory import make_recipe
+
 
 # HHTP REQUEST
 def Home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)],
+    })
 
 
 def Recipe(request, id):
-    return render(request, 'recipes/pages/recipe-views.html')
+    return render(request, 'recipes/pages/recipe-views.html', context={
+        'recipe': make_recipe(),
+    })
