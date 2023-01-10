3a213352202cc6ebf75f38f3db20e0144128d1a9
Diego Marchesi <cbdiego.marchesi@gmail.com>
2023-01-10 18:01:23 -0300

    erro ao cnectar

--- recipes/views.py
+++ recipes/views.py
@@ -3,11 +3,14 @@ from django.shortcuts import render
 # import fake para criação altomatica e testes
 from utils.recipes.factory import make_recipe
 
+from .models import Recipe
+
 
 # HHTP REQUEST
 def Home(request):
+    recipes = Recipe.objects.all().order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
     })
 
 
